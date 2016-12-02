12f527e9f8b8c2d2381695605cc7559a86d4f694
Hayden Du <haydend@nvidia.com>
2016-12-02 15:24:26 +0800

    ib_py: add init version NXPI auto trade script

---
+++ NXPI_auto_trade.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from time import sleep
+from ib.opt import Connection, message
+from ib.ext.Contract import Contract
+from ib.ext.Order import Order
+from ib.ext.CommissionReport import CommissionReport
+from ib.ext.TickType import TickType as tt
+
+def execDetails(msg):
+    print('ID',msg.execution.m_execId,'PRICE',msg.execution.m_price)
+
+def commReport(msg):
+    print('ID',msg.commissionReport.m_execId,'COM',msg.commissionReport.m_commission)
+
+def error_handler(msg):
+    print (msg)
+
+def make_contract(symbol, sec_type, exch, prim_exch, curr):
+    Contract.m_symbol = symbol
+    Contract.m_secType = sec_type
+    Contract.m_exchange = exch
+    Contract.m_primaryExch = prim_exch
+    Contract.m_currency = curr
+    return Contract
+
+def make_order(action, quantity, price = None):
+    if price is not None:
+        order = Order()
+        order.m_orderType = 'LMT'
+        order.m_totalQuantity = quantity
+        order.m_action = action
+        order.m_lmtPrice = price
+    else:
+        order = Order()
+        order.m_orderType = 'MKT'
+        order.m_totalQuantity = quantity
+        order.m_action = action
+    return order
+
+
+cid = 510
+
+while __name__ == "__main__":
+    conn = Connection.create(port=7496, clientId=100)
+    conn.register(execDetails, message.execDetails)
+    conn.register(commReport, message.commissionReport)    
+    conn.register(error_handler, 'Error')
+    conn.connect()
+
+    oid = cid
+    cont = make_contract('NXPI', 'STK', 'SMART', 'SMART', 'USD')
+    offer = make_order('BUY', 100, 99)
+    conn.placeOrder(oid, cont, offer)
+    sleep(1)
+    conn.disconnect()
+    x = raw_input('enter to resend')
+    cid += 1
